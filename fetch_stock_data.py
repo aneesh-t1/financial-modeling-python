b0914e2a6360d820e1e20f633f0492e984a20e32
Aneesh Tuggudem <76018314+aneesh-t1@users.noreply.github.com>
2025-02-12 01:14:43 +0000

    Added script to fetch stock data

---
+++ fetch_stock_data.py
@@ -0,0 +1,10 @@
+import yfinance as yf
+import pandas as pd
+
+# This will get data for a stock (can be historical)
+ticker = "AAPL"
+stock_data = yf.download(ticker, start = "2023-01-01", end = "2024-01-01", timeout = 60)
+
+stock_data.to_csv(f"{ticker}_stock_data.csv")
+
+print(f"Downloaded {ticker} stock data and saved it as {ticker}_stock_data.csv")
